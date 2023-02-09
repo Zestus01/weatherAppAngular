efe1ce4ab7bd0aa8c88f354aaa751414e823f21e
Zach Estus <zachary.estus@student.awesomeinc.org>
2023-02-09 18:38:38 +0000

    Fixing some errors, made WeatherInfo Interfacce:

--- weatherAngular/src/app/weather-display/weather-display.component.ts
+++ weatherAngular/src/app/weather-display/weather-display.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { WeatherInfo } from 'src/weatherInfo';
 
 @Component({
   selector: 'app-weather-display',
@@ -6,32 +7,60 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./weather-display.component.css']
 })
 export class WeatherDisplayComponent {
-  @Input() info: any | {};
+  @Input() weatherInfo: any | {};
   fahrenOrCelsius: boolean;
+  currentTempScale: string | undefined;
+  info: WeatherInfo = {
+    temp : {
+      max: 0,
+      min: 0,
+      curr: 0,
+    },
+    main: {},
+    wind: {},
+    name: '',
+  }
+
+
 
   constructor(){
-    this.info = "";
-    this.info.main = {};
-    this.info.main.temp = 0;
     this.fahrenOrCelsius = true;
+    this.currentTempScale = "Fahrenheit";
+    this.info.temp.max = this.weatherInfo.main.temp_max;
+    this.info.temp.min = this.weatherInfo.main.temp_min;
+    this.info.temp.curr = this.weatherInfo.main.temp;
    }
 
+
    convertTempF(): void{
-      this.info.temp.max = (this.info.main.temp_max - 273.15) * (9/5) + 32;
-      this.info.temp.min = (this.info.main.temp_min - 273.15) * (9/5) + 32;
-      this.info.temp.curr = (this.info.main.temp - 273.15) * (9/5) + 32;
+    // this.info.temp.max = 0;
+    // this.info.temp.min = 0;
+    // this.info.temp.curr = 0;
+    this.info.temp.max = (this.info.main.temp_max - 273.15) * (9/5) + 32;
+    this.info.temp.min = (this.info.main.temp_min - 273.15) * (9/5) + 32;
+    this.info.temp.curr = (this.info.main.temp - 273.15) * (9/5) + 32;
    }
 
    convertTempC(): void{
-    this.info.temp.max
+    // console.log(this.info);
+    // console.log(this.info.temp);
+    // this.info.temp.max = 0;
+    // this.info.temp.min = 0;
+    // this.info.temp.curr = 0;
+    this.info.temp.max = (this.info.main.temp_max - 273.15);
+    this.info.temp.min = (this.info.main.temp_min - 273.15);
+    this.info.temp.curr = (this.info.main.temp - 273.15);
    }
 
    changeTempScale(): void{
     this.fahrenOrCelsius = !this.fahrenOrCelsius;
     if(this.fahrenOrCelsius){
       this.convertTempF();
+      this.currentTempScale = "Fahrenheit";
     } else{
       this.convertTempC();
+      this.currentTempScale = "Celsius"
     }
+    console.log(this.currentTempScale);
    }
 }
---
+++ weatherAngular/src/weatherInfo.ts
@@ -0,0 +1,10 @@
+export interface WeatherInfo{
+    temp: {
+        max: number,
+        min: number,
+        curr: number,
+    },
+    main: {},
+    wind: {},
+    name: string,
+}
