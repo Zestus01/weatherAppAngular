63bb9d59cccc0cc30200a433a58eee1fb0a42f84
Zach Estus <zachary.estus@student.awesomeinc.org>
2023-02-09 15:55:47 +0000

    Weather Display and changing the input box text changes an h3 tag

--- weatherAngular/src/app/app.module.ts
+++ weatherAngular/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule} from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { ZipComponent } from './zip/zip.component';
+import { WeatherDisplayComponent } from './weather-display/weather-display.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ZipComponent
+    ZipComponent,
+    WeatherDisplayComponent
   ],
   imports: [
     BrowserModule,
